ca1a944bdf713ceef9b8ddb45b2ae1d400ff09cf
jcnorby <jnorby@andrew.cmu.edu>
2023-11-02 17:12:13 -0400

    MPC somewhat working, need to fix the visualization

--- ICRA_2024/multi_modal_agents/run_experiment.py
+++ ICRA_2024/multi_modal_agents/run_experiment.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 from uncontrolled_agent import UncontrolledAgent
 
 if __name__ == "__main__":
-    initial_states = [[0.0, 0.0, 0.0]]
-    final_states = [[5.0, 0.0, 0.0]]
+    initial_states = [[5.0, 0.0, 0.0]]
+    final_states = [[0.0, 2.0, 0.0]]
 
     cost_func_params = {
         'Q':  np.array([[7.0, 0.0, 0.0], [0.0, 7.0, 0.0], [0.0, 0.0, 2.5]]),
@@ -18,11 +18,11 @@ if __name__ == "__main__":
     mpc_params = {
         'num_agents': 1,
         'dt': 0.05,
-        'N' : 60,
+        'N' : 40,
         'rob_dia': 0.3,
         'v_lim': 1.0,
         'omega_lim': 1.0,
-        'total_sim_timestep': 500,
+        'total_sim_timestep': 200,
         'obs_sim_timestep': 100,
         'epsilon_o': 0.05,
         'epsilon_r': 0.05,
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     trial = 1
 
     uncontrolled_agent = UncontrolledAgent()
-    uncontrolled_traj = uncontrolled_agent.simulate_diff_drive()
+    predictions, uncontrolled_traj = uncontrolled_agent.simulate_diff_drive()
 
     mpc = MPC(initial_states, final_states, cost_func_params, obs, mpc_params, scenario, trial, uncontrolled_agent, uncontrolled_traj, map=map)
     mpc.simulate()
