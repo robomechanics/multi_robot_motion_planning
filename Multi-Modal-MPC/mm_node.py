88dbfe7372c139941bfa6fdc03eee491ee8f1115
shn66 <siddharth_nair@berkeley.edu>
2024-02-09 07:31:23 -0800

    conflict free

--- Multi-Modal-MPC/mm_node.py
+++ Multi-Modal-MPC/mm_node.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class MM_Node:
-    def __init__(self, node_id:int, mm_sol:dict, conflicts:list, resolved: dict):
+    def __init__(self, node_id:int, mm_sol:dict, conflicts:list, resolved: dict, collision_prob:float=1.0):
         '''
         mm_sol :  multi-modal trajectories that resolve the conflicts in resolved_conflicts
         resolved: branch id -> List of obstacle mode tuples indicating which obstacles and their modes are resolved
@@ -12,21 +12,22 @@ class MM_Node:
         self.mm_sol = mm_sol
         self.conflicts = conflicts
         self.resolved = resolved
+        self.collision_prob = collision_prob
         
     
         
-        def get_conflicts(self):
-                return NotImplementedError
-            
-        def update_node(self, new_conflicts, new_resolved):
+    def get_conflicts(self):
             return NotImplementedError
         
-        def _make_branch_id(self):
-            return NotImplementedError
+    def update_node(self, new_conflicts, new_resolved):
+        return NotImplementedError
+    
+    def _make_branch_id(self):
+        return NotImplementedError
+    
+    def is_conflict_free(self):
         
-        def _is_node_conflict_free(self):
-            
-            return len(self.conflicts)==0
+        return len(self.conflicts)==0
         
         
         
